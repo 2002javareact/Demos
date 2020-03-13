10b58d5597d3ec628cf1dc83214e88d5e83ba36e
Abatson <abatson94@gmail.com>
2020-03-13 17:15:37 -0400

    update cors

--- 1Week/webflicks/src/middleware/cors-filter.ts
+++ 1Week/webflicks/src/middleware/cors-filter.ts
@@ -5,6 +5,7 @@ export function corsFilter(req:Request,res:Response,next){
     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)//this is a hack, never do it in a real application
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
     res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE')
     if(req.method === 'OPTIONS'){
         //this is where we send the 'Pre Flight Response
         res.sendStatus(200)
